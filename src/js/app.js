faab29adadbff670ddb1676ce126136d7923876c
Sakil Jomadder <sakilorb@icloud.com>
2021-09-19 22:48:24 +0600

    version 1.0.0 reaseing now, few more changes will be coming soon

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,20 @@
+/* Navbar toggle with hamburger menu */
+const hamburger = document.querySelector(".hamburger .icon");
+const navbar = document.querySelector("#navbar");
+const goToTopBtn = document.querySelector("#goToTop");
+
+hamburger.onclick = function (event) {
+  event.preventDefault();
+  console.log("clicked");
+  this.classList.toggle("fa-times");
+  if (this.classList.contains("fa-times")) {
+    navbar.classList.add("active");
+  } else {
+    navbar.classList.remove("active");
+  }
+};
+/* Navbar toggle with hamburger menu end */
+
 /* Top products swiper slider */
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
@@ -50,3 +67,33 @@ function _viewportHeightCalc() {
   return viewportHeight;
 }
 /* Deal Parallax end */
+
+/* window resize */
+window.onresize = function (event) {
+  if (screen.width >= 768) {
+    hamburger.classList.remove("fa-times");
+    navbar.classList.remove("active");
+  }
+};
+/* window resize */
+
+/* window onScroll */
+window.onscroll = function (event) {
+  let scrollTopPosAfter = 200;
+  if (
+    document.body.scrollTop > scrollTopPosAfter ||
+    document.documentElement.scrollTop > scrollTopPosAfter
+  ) {
+    goToTopBtn.classList.add("active");
+  } else {
+    goToTopBtn.classList.remove("active");
+  }
+};
+
+/* window onScroll end */
+/* goToTop */
+goToTopBtn.onclick = function (event) {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
+/* goToTop end */
